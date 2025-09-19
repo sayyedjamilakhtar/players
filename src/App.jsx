71c78801a587549fb3a8a3ffc829c3f33ae38ba4
sayyedjamilakhtar <sayyedjamilakhtar@gmail.com>
2025-09-20 03:11:38 +0530

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Home from "./components/Home";
 
 function App() {
   return (
     <>
       <h1 className="text-[2rem]">App</h1>
+      <Home />
     </>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+  return <div className="text-[10rem]">Home</div>;
+}
+
+export default Home;
